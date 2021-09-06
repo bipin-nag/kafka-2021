201a6a09db6174227b076e97d3975925a1630d51
Bipin Nag <bipin.nag@gmail.com>
2021-09-06 06:28:54 +0530

    Add nest kafka-producer

---
+++ packages/nest/kafka-producer/src/app.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get } from '@nestjs/common';
+import { Client, ClientKafka, Transport } from "@nestjs/microservices";
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'kafkaSample',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'my-kafka' // Should be the same thing we give in consumer
+      }
+    }
+  })
+  client: ClientKafka;
+
+  async onModuleInit() {
+    // Need to subscribe to topic 
+    // so that we can get the response from kafka microservice
+    this.client.subscribeToResponseOf('my-first-topic');
+    await this.client.connect();
+  }
+
+  @Get()
+  getHello() {
+    console.log("Message sent");
+    return this.client.send('my-first-topic', 'Hello Kafka'); // args - topic, message
+  }
+}
